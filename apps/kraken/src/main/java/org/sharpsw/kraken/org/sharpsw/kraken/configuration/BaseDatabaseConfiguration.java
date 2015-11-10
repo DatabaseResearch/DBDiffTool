bf98b2dda70667eac161f79edf62a2a08432853e
andersonkmi <andersonkmi@acm.org>
2015-11-09 22:41:14 -0200

    Initial database configuration base class

---
+++ apps/kraken/src/main/java/org/sharpsw/kraken/org/sharpsw/kraken/configuration/BaseDatabaseConfiguration.java
@@ -0,0 +1,59 @@
+package org.sharpsw.kraken.org.sharpsw.kraken.configuration;
+
+public abstract class BaseDatabaseConfiguration {
+    private String host;
+    private Integer port;
+    private String user;
+    private String password;
+    private String schema;
+
+    public BaseDatabaseConfiguration() {
+        host = "";
+        port = 0;
+        user = "";
+        password = "";
+        schema = "";
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public abstract String getConnectionString();
+}
