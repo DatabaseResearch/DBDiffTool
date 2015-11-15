fa8f071d338b30f6ec72c3d4a97cd57dfeef7b9d
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2015-11-15 19:16:55 -0200

    Database connection factory class

--- apps/kraken/src/main/java/org/sharpsw/kraken/connectivity/DatabaseConnectionFactory.java
+++ apps/kraken/src/main/java/org/sharpsw/kraken/connectivity/DatabaseConnectionFactory.java
@@ -1,7 +1,29 @@
 package org.sharpsw.kraken.connectivity;
 
+import org.apache.log4j.Logger;
+import org.sharpsw.kraken.configuration.DatabaseConfiguration;
 import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Component
 public class DatabaseConnectionFactory {
+    private static final Logger logger = Logger.getLogger(DatabaseConnectionFactory.class);
+
+    public Connection createConnection(DatabaseConfiguration configuration) throws DatabaseConnectionException {
+        if(logger.isInfoEnabled()) {
+            logger.info("Obtaining a new database connection.");
+        }
+
+        try {
+            Class.forName(configuration.getConnectionString());
+            return DriverManager.getConnection(configuration.getConnectionString());
+        } catch (SQLException | ClassNotFoundException exception) {
+            logger.error(String.format("Error when creating a new database connection: %s", exception.getMessage()), exception);
+
+            throw new DatabaseConnectionException(String.format("Error when creating a new connection to the database: %s", exception.getMessage()), exception);
+        }
+    }
 }
