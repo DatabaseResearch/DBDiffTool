33c94ccffe76c4ce01c4afd3b4b0658a4a28167b
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2015-12-06 12:29:52 -0200

    More unit tests.

--- apps/kraken/src/test/java/org/sharpsw/kraken/service/SchemaLoaderTestCase.java
+++ apps/kraken/src/test/java/org/sharpsw/kraken/service/SchemaLoaderTestCase.java
@@ -1,14 +1,15 @@
 package org.sharpsw.kraken.service;
 
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sharpsw.kraken.configuration.DatabaseConfiguration;
 import org.sharpsw.kraken.configuration.MySQLConfiguration;
 import org.sharpsw.kraken.connectivity.DatabaseConnectionException;
+import org.sharpsw.kraken.data.Column;
 import org.sharpsw.kraken.data.Database;
+import org.sharpsw.kraken.data.SQLDataType;
 import org.sharpsw.kraken.data.Table;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,6 +17,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.annotation.Resource;
 import java.sql.SQLException;
 import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.sharpsw.kraken.data.SQLDataType.BIGINT;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:**/applicationContext.xml"})
@@ -40,24 +45,29 @@ public class SchemaLoaderTestCase {
     @Test
     public void testLoadEmptySchemaOK() throws SQLException, DatabaseConnectionException, SchemaLoaderException {
         Database database = schemaLoader.load(configuration);
-        Assert.assertThat(database.getProductName(), Matchers.is("MySQL"));
-        Assert.assertThat(database.getSchema(), Matchers.is("dbdiff"));
-        Assert.assertThat(database.getTables(), Matchers.empty());
-        Assert.assertThat(database.getViews(), Matchers.empty());
+        assertThat(database.getProductName(), is("MySQL"));
+        assertThat(database.getSchema(), is("dbdiff"));
+        assertThat(database.getTables(), empty());
+        assertThat(database.getViews(), empty());
     }
 
     @Test
     public void testLoadDBDiffUT1SchemaOK() throws SQLException, DatabaseConnectionException, SchemaLoaderException {
         configuration.setSchema("dbdiffut1");
         Database database = schemaLoader.load(configuration);
-        Assert.assertThat(database.getProductName(), Matchers.is("MySQL"));
-        Assert.assertThat(database.getSchema(), Matchers.is("dbdiffut1"));
-        Assert.assertThat(database.getTables(), Matchers.not(Matchers.empty()));
-        Assert.assertThat(database.getViews(), Matchers.empty());
+        assertThat(database.getProductName(), is("MySQL"));
+        assertThat(database.getSchema(), is("dbdiffut1"));
+        assertThat(database.getTables(), not(empty()));
+        assertThat(database.getViews(), empty());
 
         List<Table> tables = database.getTables();
         Table table = tables.get(0);
-        Assert.assertThat(table.getName(), Matchers.is("table001"));
-        Assert.assertThat(table.getColumns(), Matchers.not(Matchers.empty()));
+        assertThat(table.getName(), is("table001"));
+        assertThat(table.getColumns(), not(empty()));
+        assertThat(table.getColumns().size(), equalTo(2));
+
+        Column col1 = table.getColumns().get(0);
+        assertThat(col1.getName(), is("id"));
+        assertThat(col1.getDataType(), is(BIGINT));
     }
 }
